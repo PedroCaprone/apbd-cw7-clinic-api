5d700aa1a13ffaf1edf8226dfcea83bbfa37803c
PedroCaprone <129163662+PedroCaprone@users.noreply.github.com>
2026-04-29 20:54:34 +0200

    Add apoinment DTOs and project structure

---
+++ apbd-cw7-clinic-api/Controllers/AppointmentsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_cw7_clinic_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentsController : ControllerBase
+{
+    
+}
---
+++ apbd-cw7-clinic-api/DTOs/AppointmentDetailsDto.cs
@@ -0,0 +1,6 @@
+namespace apbd_cw7_clinic_api.DTOs;
+
+public class AppointmentDetailsDto
+{
+    
+}
